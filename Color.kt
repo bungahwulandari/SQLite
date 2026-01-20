b21b0527d02f724e11cc8f8fde270a9e6cdf28a9
bungahwulandari <bungahwlndr@gmail.com>
2026-01-20 20:03:32 +0700

    Create Color.kt

---
+++ Color.kt
@@ -0,0 +1,9 @@
+package com.example.sqlite
+
+import android.graphics.Color
+
+object AppColors {
+    val PRIMARY = Color.parseColor("#4CAF50")
+    val TEXT = Color.parseColor("#212121")
+    val WHITE = Color.WHITE
+}
